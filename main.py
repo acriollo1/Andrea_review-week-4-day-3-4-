ddf9517285cbb88ff4cdb791dcea40337bba6577
Andrea Criollo <113551844+acriollo1@users.noreply.github.com>
2022-09-16 19:50:04 +0000

    the list challenges

--- main.py
+++ main.py
@@ -40,34 +40,39 @@ luckyNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15]
 friends = ["Kevin", "Karen", "jim", "oscar", "tim", "lord tennyson"]
 # instructions:
 ######join both lists together in a new list
-
+newList2 = luckyNumbers + friends
 ###### print the new list out
-
+print(newList2)
 ###### print out the first item in the list
-
+print(newList2[0])
 ###### remove the last item of the new list
-
+newList2.pop()
 ###### print the new list out without the last item
-
+print(newList2)
 ###### add three more items to the end of the list
-
+newList2.append("May")
+newList2.append("Kate")
+newList2.append("Hannah")
+print(newList2)
 ###### print the last item in the new list
-
+newList2.pop()
+print(newList2)
 ###### sort the list
-
+print(friends.sort())
 ###### find a way to insert a new item at the 3rd position of the list
-
+newList2.insert(2, "leo")
 ###### print the new list out
-
+print(newList2)
 ##### reverse the list
-
+newList2.reverse()
 ###### print the new list out
+print(newList2)
 ###### print out the length of the new list
-
+print(len(newList2))
 #####################################challenge 2#######################################################
 # Join the following list into a string, separating each item with a space. Use the appropriate list/string method, and display the result.
 word_list = ["Simple", "is", "better", "than", "complex."]
-
+print(' '. join(word_list))
 # Print the following text in uppercase, using the specific string method:
 
 text1 = "Especially in electronic communications, writing in all caps is equivalent to yelling."
